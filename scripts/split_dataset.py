e388c9303ddf8877757af41998f4713d2276d24a
maciej-ol <maciekk.oldakowskii@gmail.com>
2024-03-08 17:36:53 +0100

    add split data folder

--- scripts/split_dataset.py
+++ scripts/split_dataset.py
@@ -37,8 +37,8 @@ def split_datasets(
 if __name__ == "__main__":
     split_datasets(
         "data/processed/processed_data.csv",
-        "data/processed/processed_data_train.csv",
-        "data/processed/processed_data_test.csv",
-        "data/processed/processed_data_validate.csv",
+        "data/split/train_data.csv",
+        "data/split/test_data.csv",
+        "data/split/validate_data.csv",
         random_state=47,
     )
