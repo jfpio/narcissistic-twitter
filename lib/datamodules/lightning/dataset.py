e59020e68be3ab12d5e7cb7d9815eea7f8b7c540
OhDuck47 <kp.oldakowsk@student.uw.edu.pl>
2024-08-06 14:34:17 +0200

    fixed error or unused notebooks

--- lib/datamodules/lightning/dataset.py
+++ lib/datamodules/lightning/dataset.py
@@ -29,7 +29,7 @@ class NarcissisticPostDataset(Dataset):
         label = torch.tensor(label, dtype=torch.float)
 
         return {
-            "input_ids": encoding["input_ids"].flatten(), #if flatten it won't work
+            "input_ids": encoding["input_ids"].flatten(),
             "attention_mask": encoding["attention_mask"].flatten(),
             "labels": label,
         }
